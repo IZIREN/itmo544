bc7ea73ecfa4760f118dd36778915ee027d632ca
maxislash <descos.maxime@gmail.com>
2016-09-30 02:56:40 +0000

    ./create-env.sh ami-06b94666 and ./destroy-env.sh work

--- create-env.sh
+++ create-env.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-DATE=`date +%Y-%m-%d:%H:%M:%S`
-TOKEN=$DATE'-itmo544-mdescos'
-echo $TOKEN
-
-aws ec2 run-instances --image-id ami-06b94666 --key-name macmax3 --security-group-ids sg-1a39c263 --instance-type t2.micro --count 3 --user-data file://installenv.sh --placement AvailabilityZone=us-west-2b --client-token $TOKEN
-
-ID=`aws ec2 describe-instances --filters "Name=client-token,Values=$TOKEN" --query 'Reservations[*].Instances[].InstanceId'`
-echo $ID
-
-aws ec2 wait instance-running --instance-ids $ID --filters "Name=client-token,Values=$TOKEN"
-
+echo "DNS name of the load balancer :"
 SUBNET=`aws ec2 describe-subnets --filters "Name=availabilityZone,Values=us-west-2b" --query 'Subnets[*].SubnetId'`
 aws elb create-load-balancer --load-balancer-name itmo544-mdescos --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets $SUBNET --security-groups sg-1a39c263
 
-aws elb register-instances-with-load-balancer --load-balancer-name itmo544-mdescos --instances $ID
-
-aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id ami-06b94666 --key-name macmax3 --instance-type t2.micro --user-data file://installenv.sh  --security-groups sg-1a39c263
+aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id $1 --key-name macmax3 --instance-type t2.micro --user-data file://installenv.sh  --security-groups sg-1a39c263
 
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name webserverdemo --launch-configuration webserver --availability-zone us-west-2b --max-size 5 --min-size 2 --desired-capacity 4
 
 aws autoscaling attach-load-balancers --auto-scaling-group-name webserverdemo --load-balancer-names itmo544-mdescos
+
+echo "All done"
--- destroy-env.sh
+++ destroy-env.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
 GROUPNAME=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName'`
-echo $GROUPNAME
+echo "Autoscaling group name: " $GROUPNAME
 LOADBALANCERNAME=`aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].LoadBalancerNames'`
-echo $LOADBALANCERNAME
+echo "Loadbalancer name: " $LOADBALANCERNAME
 CONFIGURATIONNAME=`aws autoscaling describe-launch-configurations --query 'LaunchConfigurations[*].LaunchConfigurationName'`
-echo $CONFIGURATIONNAME
+echo "Launch configuration name: " $CONFIGURATIONNAME
 ID=`aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[].InstanceId'`
-echo $ID
+echo "Ids of instances on the autocaling group: " $ID
 PORT=`aws elb describe-load-balancers --load-balancer-names $LOADBALANCERNAME --query 'LoadBalancerDescriptions[*].ListenerDescriptions[*].Listener[].LoadBalancerPort'`
-echo $PORT
-POLICYNAME=`aws elb describe-load-balancer-policies --load-balancer-name $LOADBALANCERNAME --query 'PolicyDescriptions[*].PolicyName'`
+echo "Listener port of the load balancer: " $PORT
 
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name $GROUPNAME --launch-configuration-name $CONFIGURATIONNAME --min-size 0 --max-size 0 --desired-capacity 0
-aws autoscaling detach-load-balancers --auto-scaling-group-name $GROUPNAME --load-balancer-names $LOADBALANCERNAME
+echo "Wait for instances to be terminated"
 aws ec2 wait instance-terminated --instance-ids $ID
+
+aws autoscaling detach-load-balancers --auto-scaling-group-name $GROUPNAME --load-balancer-names $LOADBALANCERNAME
+
+aws elb delete-load-balancer-listeners --load-balancer-name $LOADBALANCERNAME --load-balancer-ports $PORT
+aws elb  delete-load-balancer --load-balancer-name $LOADBALANCERNAME
 
 aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $GROUPNAME
 
 aws autoscaling delete-launch-configuration --launch-configuration-name $CONFIGURATIONNAME
 
-aws elb deregister-instances-from-load-balancer --load-balancer-name $LOADBALANCERNAME --instances $ID
-
-aws elb delete-load-balancer-listeners --load-balancer-name $LOADBALANCERNAME --load-balancer-ports $PORT
-aws elb delete-load-balancer-policy --load-balancer-name $LOADBALANCERNAME --policy-name $POLICYNAME
-
-aws elb  delete-load-balancer --load-balancer-name $LOADBALANCERNAME
-
-aws ec2 terminate-instances --instance-ids $ID
-
+echo "All done"
