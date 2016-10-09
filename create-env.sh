656f69e29d018a2ae943ee80f7c2b2c62f164412
maxislash <descos.maxime@gmail.com>
2016-10-09 20:41:20 +0000

    Positional Parameters Added

--- create-env.sh
+++ create-env.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-echo "DNS name of the load balancer :"
-SUBNET=`aws ec2 describe-subnets --filters "Name=availabilityZone,Values=us-west-2b" --query 'Subnets[*].SubnetId'`
-aws elb create-load-balancer --load-balancer-name itmo544-mdescos --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets $SUBNET --security-groups sg-1a39c263
+if [ $# != 5 ]
+then
+	echo "Not the proper amount of parameters: 5 needed"
+else
+	AMIID=$1
+	KEYNAME=$2
+	SECURITYGROUP=$3
+	LAUNCHCONFIGURATION=$4
+	COUNT=$5
 
-aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id $1 --key-name macmax3 --instance-type t2.micro --user-data file://installenv.sh  --security-groups sg-1a39c263
+	echo "DNS name of the load balancer :"
+	SUBNET=`aws ec2 describe-subnets --filters "Name=availabilityZone,Values=us-west-2b" --query 'Subnets[*].SubnetId'`
+	aws elb create-load-balancer --load-balancer-name itmo544-mdescos --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets $SUBNET --security-groups $SECURITYGROUP
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name webserverdemo --launch-configuration webserver --availability-zone us-west-2b --max-size 5 --min-size 2 --desired-capacity 4
+	aws autoscaling create-launch-configuration --launch-configuration-name $LAUNCHCONFIGURATION --image-id $AMIID --key-name $KEYNAME --instance-type t2.micro --user-data file://installenv.sh  --security-groups $SECURITYGROUP
 
-aws autoscaling attach-load-balancers --auto-scaling-group-name webserverdemo --load-balancer-names itmo544-mdescos
+	aws autoscaling create-auto-scaling-group --auto-scaling-group-name webserverdemo --launch-configuration $LAUNCHCONFIGURATION --availability-zone us-west-2b --max-size 5 --min-size 2 --desired-capacity $COUNT
 
-echo "All done"
+	aws autoscaling attach-load-balancers --auto-scaling-group-name webserverdemo --load-balancer-names itmo544-mdescos
+
+	echo "All done"
+fi
